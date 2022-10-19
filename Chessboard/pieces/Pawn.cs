9eeab2d222bcf00dc43b3e3ff5935304e77d521c
benlamouche <benlamouche78@hotmail.fr>
2022-10-19 20:23:44 +0200

    pawn test

---
+++ Chessboard/pieces/Pawn.cs
@@ -0,0 +1,23 @@
+﻿
+namespace chessboard.pieces
+{
+    public class Pawn : PieceBase, IPiece
+    {
+        public Pawn(enums.Color color = enums.Color.White) : base("", "", color) { }
+
+
+        public Pawn(string collumn, string row, enums.Color color = enums.Color.White) : base(collumn, row, color) { }
+
+        public string Name { get => "Pawn"; }
+
+        public List<Square> AvailableMove
+        {
+            get
+            {
+                var moves = new List<Square>();
+
+                return moves;
+            }
+        }
+    }
+}
---
+++ ChessboardTest/tests/PawnTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using chessboard.pieces;
+using chessboard.enums;
+using chessboard;
+
+namespace chessboardTest
+{
+    [TestClass]
+    public class PawnTest
+    {
+        [TestMethod]
+        public void PawnCreation()
+        {
+            var pawn = new Pawn();
+            Assert.IsNotNull(pawn);
+            Assert.AreEqual(pawn.Name, "Pawn");
+        }
+
+        [TestMethod]
+        public void PawnMove1()
+        {
+            var chessboard = new Chessboard();
+
+            var pawn = new Pawn("d", "5",Color.White);
+            chessboard.AddPiece(pawn);
+
+            Assert.IsTrue(pawn.AvailableMove.Count == 1);
+            Assert.IsTrue(pawn.AvailableMove.Contains(new Square("d", "6")));
+        }
+
+        [TestMethod]
+        public void PawnMove2()
+        {
+            var chessboard = new Chessboard();
+
+            var pawn = new Pawn("d", "5",Color.Black);
+            chessboard.AddPiece(pawn);
+
+            Assert.IsTrue(pawn.AvailableMove.Count == 1);
+            Assert.IsTrue(pawn.AvailableMove.Contains(new Square("d", "4")));
+        }
+    }
+}
