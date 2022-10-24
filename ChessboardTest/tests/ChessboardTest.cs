e17e3d7618813311e7d88298e21dd99f30a0263f
idriss benhamadouche <idriss.benhamadouche@laposte.net>
2022-10-24 21:30:32 +0200

    Knight creation

--- ChessboardTest/tests/ChessboardTest.cs
+++ ChessboardTest/tests/ChessboardTest.cs
@@ -20,8 +20,23 @@ namespace chessboardTest
         {
             var chessboard = new Chessboard();
             chessboard.NewGame();
-            var king = chessboard.GetSquare("e", "1");
-            Assert.IsTrue(king is King);
+
+            var whiteRook1 = chessboard.GetSquare("a", "1");
+            Assert.IsTrue(whiteRook1 is Rook);
+            var whiteKnight1 = chessboard.GetSquare("b", "1");
+            Assert.IsTrue(whiteKnight1 is Knight);
+            var whiteBishop1 = chessboard.GetSquare("c", "1");
+            Assert.IsTrue(whiteBishop1 is Bishop);
+            var whiteQueen = chessboard.GetSquare("d", "1");
+            Assert.IsTrue(whiteQueen is Queen);
+            var whiteKing = chessboard.GetSquare("e", "1");
+            Assert.IsTrue(whiteKing is King);
+            var whiteBishop2 = chessboard.GetSquare("f", "1");
+            Assert.IsTrue(whiteBishop2 is Bishop);
+            var whiteKnight2 = chessboard.GetSquare("g", "1");
+            Assert.IsTrue(whiteKnight2 is Knight);
+            var whiteRook2 = chessboard.GetSquare("h", "1");
+            Assert.IsTrue(whiteRook2 is Rook);
         }
 
         [TestMethod]
---
+++ ChessboardTest/tests/KnightTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using chessboard.pieces;
+using chessboard.enums;
+using chessboard;
+
+namespace chessboardTest
+{
+    [TestClass]
+    public class KnightTest
+    {
+        [TestMethod]
+        public void KnightCreation()
+        {
+            var knight = new Knight();
+            Assert.IsNotNull(knight);
+            Assert.AreEqual(knight.Name, "Knight");
+        }
+    }
+}
